0aa32e7ed6bec05e0e0f21d7d1c53d0c6e7911d6
Alex Bruckner <Alexander.Bruckner@semantico.com>
2013-12-18 17:10:05 +0000

    adding data to execute methods + provide action perform method (to do block update capability to data)

--- src/main/scala/ax/bru/Action.scala
+++ src/main/scala/ax/bru/Action.scala
@@ -1,23 +1,32 @@
 package ax.bru
 
-class Action(override val name: String, val parallel: Boolean, val steps: Seq[Step]) extends Step(name)({}) {
+class Action(override val name: String, val steps: Seq[Step], val parallel: Boolean) extends Step(name)({}) {
 
-  override def execute() = {
+  val data: Map[String, Any] = Map[String, Any]()
+
+  def perform() = {
+    execute(this)
+  }
+
+  override def execute(action: Action) = {
     println("Running " + (if (parallel) "parallel " else "") + "action: " + name)
     if (parallel) {
-      steps.reverse.foreach(_.execute())  // for now use reverse to indicate parallelism for later
+      steps.reverse.foreach(_.execute(action))  // for now use reverse to indicate parallelism for later
     } else {
-      steps.foreach(_.execute())
+      steps.foreach(_.execute(action))
     }
+    println("Finished " + (if (parallel) "parallel " else "") + "action: " + name)
   }
 }
 
 object Action {
-  def apply(name: String, parallel: Boolean = false)(steps: Step*):Action = new Action(name, parallel, steps)
+  def apply(name: String, parallel: Boolean = false)(steps: Step*):Action = {
+    new Action(name, steps, parallel)
+  }
 }
 
 class Step(val name: String)(block: => Unit){
-  def execute() = {
+  def execute(action: Action) = {
     println("Executing step: " + name)
     block
   }
@@ -47,6 +56,6 @@ object ActionMain extends App {
 
   val anotherAction = Action(name = "Action for STEP 7 and STEP 8")(step7, step8)
 
-  Action(name = "MAIN ACTION")(step1, step2, parallelSteps3To5, step6, anotherAction).execute()
+  Action(name = "MAIN ACTION")(step1, step2, parallelSteps3To5, step6, anotherAction).perform()
 
 }
