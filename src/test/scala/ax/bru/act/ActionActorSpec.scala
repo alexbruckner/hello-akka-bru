20b690e420399bcc5abb6101414684b22aa8b4cd
Alex Bruckner <Alexander.Bruckner@semantico.com>
2014-01-15 13:10:32 +0000

    test spec: check order of map values

--- src/test/scala/ax/bru/act/ActionActorSpec.scala
+++ src/test/scala/ax/bru/act/ActionActorSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import org.scalatest.matchers.ShouldMatchers
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
+import scala.collection.immutable.SortedMap
 import scala.concurrent.duration._
 
 class ActionActorSpec(_system: ActorSystem)
@@ -21,11 +22,28 @@ class ActionActorSpec(_system: ActorSystem)
   }
 
   ActionSystem.addAction(ExampleAction.action)
+  ActionSystem.perform(self, ExampleAction.action.name, ("0", 0))
+  val received: Message = receiveOne(5 seconds).asInstanceOf[Message]
 
   it should "return message to sender with 8 map entries" in {
-    ActionSystem.perform(self, ExampleAction.action.name, ("key", "value"))
-    val received: Message = receiveOne(5 seconds).asInstanceOf[Message]
     received.getAll.size == 8
+  }
+
+  it should "return map entries with keys 0 to 8" in {
+    SortedMap(received.getAll.toSeq:_*).keys.toString == "Set(0, 1, 2, 3, 4, 5, 6, 7, 8)"
+  }
+
+  it should "return map entries with timestamp values in same order as keys" in { // todo this will have to revisited once the 'parallel' case is implemented!
+    var previous: String = "0"
+    var ok: Boolean = true
+    for (i <- 0 to 8) {
+      val next = received.get(s"$i").toString
+      if (next < previous) {
+        ok = false
+      }
+      previous = next
+    }
+    ok
   }
 
 }
