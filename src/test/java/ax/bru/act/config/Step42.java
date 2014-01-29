c68487a317a62e95264cae2c3b77a01869623d2f
Alex Bruckner <Alexander.Bruckner@semantico.com>
2014-01-29 15:54:18 +0000

    fixing test

--- src/test/java/ax/bru/act/config/Step42.java
+++ src/test/java/ax/bru/act/config/Step42.java
@@ -12,6 +12,11 @@ public class Step42 implements Executable {
     @Override
     public void execute(Data data) {
         System.out.println("Step 4-2!!!");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         data.set("6", System.nanoTime());
     }
 }
